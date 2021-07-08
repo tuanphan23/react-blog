4f603cc475fece9f939b8b8bc8525a72177387d7
Tuan Phan <tphan@seattleu.edu>
2021-07-07 22:50:38 -0700

    Added setting page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Setting from "./pages/settings/Setting";
 import Write from "./pages/write/Write";
 function App() {
   return (
     <>
       <TopBar />
-      <Write />
+      <Setting />
     </>
   );
 }
