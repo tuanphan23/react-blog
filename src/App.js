5f0c687f19018e4c355099f16d44309688bc882a
Tuan Phan <tphan@seattleu.edu>
2021-07-07 22:57:02 -0700

    Added login and register page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import TopBar from "./components/topbar/TopBar";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Setting from "./pages/settings/Setting";
 import Write from "./pages/write/Write";
 function App() {
   return (
     <>
       <TopBar />
-      <Setting />
+      <Register />
     </>
   );
 }
