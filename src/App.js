cf1faf55b2951a2cb2f9113de4975c942ce96a23
Tuan Phan <tphan@seattleu.edu>
2021-07-07 23:25:25 -0700

    Added React Router Dom

--- src/App.js
+++ src/App.js
@@ -3,13 +3,31 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Setting from "./pages/settings/Setting";
+import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
+  const currentUser = true;
   return (
-    <>
+    <Router>
       <TopBar />
-      <Register />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/posts">
+          <Home />
+        </Route>
+        <Route path="/register">{currentUser ? <Home /> : <Register />}</Route>
+        <Route path="/login">{currentUser ? <Home /> : <Login />}</Route>
+        <Route path="/post/:id">
+          <Single />
+        </Route>
+        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
+        <Route path="/settings">{currentUser ? <Setting /> : <Login />}</Route>
+      </Switch>
+    </Router>
   );
 }
 
