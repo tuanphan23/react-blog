cf3b6fccae773a2fad5718b45ae08554474e88b9
Tuan Phan <tphan@seattleu.edu>
2021-07-08 23:25:39 -0700

    Updated setting page

--- client/src/pages/settings/Setting.jsx
+++ client/src/pages/settings/Setting.jsx
@@ -1,7 +1,46 @@
-import Sidebar from "../../components/sidebar/Sidebar";
 import "./setting.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { useContext, useState } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
 
-export default function Setting() {
+export default function Settings() {
+  const [file, setFile] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const { user, dispatch } = useContext(Context);
+  const PF = "http://localhost:5000/images/";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "UPDATE_START" });
+    const updatedUser = {
+      userId: user._id,
+      username,
+      email,
+      password,
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      updatedUser.profilePic = filename;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+    try {
+      const res = await axios.put("/users/" + user._id, updatedUser);
+      setSuccess(true);
+      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "UPDATE_FAILURE" });
+    }
+  };
   return (
     <div className="settings">
       <div className="settingsWrapper">
@@ -9,36 +48,50 @@ export default function Setting() {
           <span className="settingsUpdateTitle">Update Your Account</span>
           <span className="settingsDeleteTitle">Delete Account</span>
         </div>
-        <form className="settingsForm">
+        <form className="settingsForm" onSubmit={handleSubmit}>
           <label>Profile Picture</label>
           <div className="settingsPP">
             <img
-              src="https://scontent-sea1-1.xx.fbcdn.net/v/t1.6435-9/84334845_2709290712459828_345296666015825920_n.jpg?_nc_cat=102&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=lFA_FXEIw1UAX_Wjo9K&_nc_ht=scontent-sea1-1.xx&oh=715de0e3816a55b4bda83896e3b6e583&oe=60EAB94A"
+              src={file ? URL.createObjectURL(file) : PF + user.profilePic}
               alt=""
             />
             <label htmlFor="fileInput">
-              <i className="settingsPPIcon far fa-user-circle"></i>{" "}
+              <i className="settingsPPIcon far fa-user-circle"></i>
             </label>
             <input
-              id="fileInput"
               type="file"
+              id="fileInput"
               style={{ display: "none" }}
-              className="settingsPPInput"
+              onChange={(e) => setFile(e.target.files[0])}
             />
           </div>
           <label>Username</label>
-          <input type="text" placeholder="Tuan Phan" name="name" />
+          <input
+            type="text"
+            placeholder={user.username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
           <label>Email</label>
           <input
             type="email"
-            placeholder="anhtuan230595@gmail.com"
-            name="email"
+            placeholder={user.email}
+            onChange={(e) => setEmail(e.target.value)}
           />
           <label>Password</label>
-          <input type="password" placeholder="Password" name="password" />
-          <button className="settingsSubmitButton" type="submit">
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="settingsSubmit" type="submit">
             Update
           </button>
+          {success && (
+            <span
+              style={{ color: "green", textAlign: "center", marginTop: "20px" }}
+            >
+              Profile has been updated...
+            </span>
+          )}
         </form>
       </div>
       <Sidebar />
